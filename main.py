eafb25fe2e95050cad3f5ab34c59109bed82ff77
AmirHosseinBaziyar <baziyar72@gmail.com>
2023-02-10 22:27:29 +0330

    update code

--- main.py
+++ main.py
@@ -1,15 +1,6 @@
 #Mr.B (Amir Hossein Baziyar)
 #baziyar72@gmail.com
 #iran - Tehran
-#Quesion = Write a function that receives a number as an input argument,
-#          then calculates the prime numbers from 1 to that number.
-#          In the next step,    
-#          it receives a number as an input argument,
-#          then calculates its prime divisors and prints it in the output. 
-#Result =>
-#Enter your max number range for range numbers (More than the desired number) : 10
-#Enter your Number to check prime divisors : 10
-#prime divisors of 10 => [2, 5]
 
  
 ListNumbers = [] # for prime numbers range [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
@@ -17,40 +8,41 @@ PrimeNumbers = [] # for prime numbers [2, 3, 5, 7]
 PrimeDivisorsNumbers = [] # For prime divisors numbers (Number = 10) [2, 5]
 
 
-def numbers(Num): #To fill the ListNumbers
-    for x in range(2, Num+1): # Because the first  prime number is 2, then our array also starts from 2. In order to have the number entered by the user in the array, we add one 
+def numbers(Num):
+    for x in range(2, Num+1):
         ListNumbers.append(x)
 
 
-# I used the sieve algorithm for this
-
-
-def delete(x, ListNum): # get prime number and latest list number list
+def delete(x, ListNum):
     for z in ListNum:
         if z % x == 0:
-            ListNumbers.remove(z) #Remove numbers divided by the prime number
+            ListNumbers.remove(z)
     return ListNum
 
 
-def check_prime(ListNum): # get prime numbers
-    if len(ListNum) != 0: # At the end, ListNumbers[] will be empty. So if it is empty, it means there is no number to check. In other words, my exit point is empty ListNumber[]. 
-        x = ListNum[0] # The first available number is the prime number
-        PrimeNumbers.append(x) # Add prime number to PrimeNumbers list
-        NewList = delete(x, ListNum) # Call the Delete() function and assign two parameters, the last prime number and the last list of numbers.
-        check_prime(NewList) # (recursive function) After removing the remaining divisible numbers in the list, we get the new list and call check_prime() again with the "NewList" parameter.
+def check_prime(ListNum):
+    while(len(ListNum) != 0):
+        x = ListNum[0]
+        PrimeNumbers.append(x)
+        ListNum = delete(x, ListNum)
+    #if len(ListNum) != 0: 
+        #x = ListNum[0]
+        #PrimeNumbers.append(x)
+        #NewList = delete(x, ListNum)
+        #check_prime(NewList)
 
 
-def prime_divisors(num, prime): # Get target number and prime number list
+def prime_divisors(num, prime):
     for x in prime:
         if num % x == 0:
-            PrimeDivisorsNumbers.append(x) # Add the prime numbers that are divisible by the target number to "prime_divisors".
+            PrimeDivisorsNumbers.append(x)
 
 
 if __name__ == "__main__":
     MaxNumber = int(input("Enter your max number range for range numbers (More than the desired number) : "))
-    numbers(MaxNumber) # create list
-    check_prime(ListNumbers) # create prime numbers
-    # print(PrimeNumbers)
+    numbers(MaxNumber)
+    check_prime(ListNumbers)
+    print(PrimeNumbers)
     Number = int(input("Enter your Number to check prime divisors : "))
-    prime_divisors(Number, PrimeNumbers) # Get Prime Divisors Numbers List
+    prime_divisors(Number, PrimeNumbers)
     print(f"prime divisors of {Number} => {PrimeDivisorsNumbers}")
